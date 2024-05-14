6016214561c76e4a7548a1365d5fec4fb24d45a1
Deaspecty <alimzhankangerey@gmail.com>
2024-05-14 06:03:04 +0500

    yoohoo another change

--- models/user.py
+++ models/user.py
@@ -1,4 +1,3 @@
 def save_user(cursor, phone_number, name, id):
     cursor.execute("UPDATE users SET fullname=%s, phone_number=%s WHERE id=%s", (phone_number, name, id))
-    cursor.commit()
     cursor.close()
