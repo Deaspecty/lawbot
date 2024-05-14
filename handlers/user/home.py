7ece106b1f39cd9fb9aa20e9b12240d197300f2d
Deaspecty <alimzhankangerey@gmail.com>
2024-05-14 06:04:34 +0500

    another...

--- handlers/user/home.py
+++ handlers/user/home.py
@@ -12,18 +12,18 @@ router = Router()
 
 
 @router.message(Command("start"))
-async def home(message: Message, state: FSMContext, user, cursor):
-    print(user)
-    if user[1] is None:
-        await message.answer(text="Как к вам обращаться?")
-        await state.set_state(AuthState.wait_name)
-    elif user[3] is None:
-        await message.answer(text="Нажмите на кнопку \"Поделится номером\"", reply_markup=contact_keyboard())
-    else:
-        await state.set_state(HomeState.wait_home)
-        await message.answer(text="Привет я Юрист-бот! "
-                                  "\n\nВыберите категорию:"
-                                  "\nКатегория 1\nКатегория 2\nКатегория 3", reply_markup=category_keyboard(cursor))
+async def home(message: Message, state: FSMContext, cursor):
+    # print(user)
+    # if user[1] is None:
+    #     await message.answer(text="Как к вам обращаться?")
+    #     await state.set_state(AuthState.wait_name)
+    # elif user[3] is None:
+    #     await message.answer(text="Нажмите на кнопку \"Поделится номером\"", reply_markup=contact_keyboard())
+    # else:
+    await state.set_state(HomeState.wait_home)
+    await message.answer(text="Привет я Юрист-бот! "
+                              "\n\nВыберите категорию:"
+                              "\nКатегория 1\nКатегория 2\nКатегория 3", reply_markup=category_keyboard(cursor))
 
 
 @router.callback_query(HomeCallback.filter(F.action == "faq"))
